5ab34cb2167c978603e83924d97aab3196498ea5
Aaron Marshall Cohen <aaronmarshallcohen@gmail.com>
2019-06-25 20:36:51 -0700

    additions to game but  still not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,12 @@
-// You'll create a trivia form with multiple choice or true/false options (your choice).
+$(document).ready(function(){
+  $("startButton").click(function(){
+  console.log("startbutton clicked.");
 
-// * The player will have a limited amount of time to finish the quiz. 
-
-//   * The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
-
-// * Don't let the player pick more than one answer per question.
-
-// * Don't forget to include a countdown timer.
+});
+});
 
 
-//Make a function with a clickable start button to begin game
-
-//function startGame(runTimer) {
-
-
-
-
-// Create a variable to hold the questions. Create an array for each question with four possible answers. Make a variable for the correct answer using its index in the array. Include an image for the correct answer.
-var correctAnswer;
-var answerImage;
-var questions = [
-    { 
-        question: "Which album did the Radioactive Chicken Heads release on Friday the 13th of October, 2017?",
-        answers : ["Music For Mutants", "Growing Mold", "Poultry Uprising", "Tales From the Coop"],
-        correctAnswer: "Tales From the Coop",
-        answerImage: "assets/images/TalesFromTheCoop.jpg"
-    },
-    {
-        question: "Which of celebrity judge gonged the Radioactive Chicken Heads on the Gong Show?",
-        answers : ["Will Arnett", "Jennifer Aniston", "Jack Black", "Zach Galifianakis"],
-        correctAnswer: "Jack Black",
-        answerImage: "assets/images/JackBlack.jpg"
-    }
-  ];
-function start(){
-for (var i = 0; i < questions.length; i++) { 
-  $("#question1").text(questions[i].question); //display question
-    for (var j = 0; i<questions[i].answers.length; i++){
-      $("#answer1").text(questions[j].answers)
-    } 
-  }
-}
-start(); 
-
-
-// function countDown {
-
-// }
-
-var countdownNumber = 30;
+  var countdownNumber=60;
 
     //  Variable that will hold our interval ID when we execute
     //  the "run" function
@@ -66,7 +24,7 @@ function runTimer() {
     countdownNumber--;
 
     //  Show the number in the #timer tag.
-    $("#timer").html("<h2>" + countdownNumber + "</h2>");
+    $("#timer").html("<h2> Seconds left: " + countdownNumber + "</h2>");
 
 
     //  Once number hits zero...
@@ -93,4 +51,46 @@ function runTimer() {
   //  Execute the run function.
   runTimer();
 
-//};
+
+var questions = [
+    { 
+        question: "Which album did the Radioactive Chicken Heads release on Friday the 13th of October, 2017?",
+        choices: ["Music For Mutants", "Growing Mold", "Poultry Uprising", "Tales From the Coop"],
+        correctAnswer: 3,
+        answerImage: "assets/images/TalesFromTheCoop.jpg"
+    },
+    {
+        question: "Which of celebrity judge gonged the Radioactive Chicken Heads on the Gong Show?",
+        choices: ["Will Arnett", "Jennifer Aniston", "Jack Black", "Zach Galifianakis"],
+        correctAnswer: 2,
+        answerImage: "assets/images/JackBlack.jpg"
+    }
+  ];
+
+  var currentQuestion = 0;
+  var correctAnswers = 0;
+
+function start(){
+for (var i = 0; i < questions.length; i++) { 
+  $("#question1").text(questions[i].question); //display question
+    for (var j = 0; i<questions[i].choices.length; i++){
+      $("#answer1").text(questions[j].choices)
+    } 
+  }
+}
+start(); 
+
+
+
+$(document).ready(function(){
+    $('#btnGetValue').click(function() {
+        var selValue = $('input[name=rbnNumber]:checked').val(); 
+        $('p').html('<br/>Selected Radio Button Value is : <b>' + selValue + '</b>');
+    });
+});
+
+
+
+
+
+   
